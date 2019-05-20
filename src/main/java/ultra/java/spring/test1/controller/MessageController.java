ddb12881831a18f77af30292b6efbc7cff419892
goofyk <goof07@inbox.ru>
2019-05-21 00:31:56 +0300

    Initial commit

---
+++ src/main/java/ultra/java/spring/test1/controller/MessageController.java
@@ -0,0 +1,10 @@
+package ultra.java.spring.test1.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("message")
+public class MessageController {
+}
