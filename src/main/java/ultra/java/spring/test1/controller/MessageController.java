77dcf67356e14973672746a79c5755830d399e1a
goofyk <goof07@inbox.ru>
2019-05-21 00:53:56 +0300

    Update methods List

--- src/main/java/ultra/java/spring/test1/controller/MessageController.java
+++ src/main/java/ultra/java/spring/test1/controller/MessageController.java
@@ -1,10 +1,29 @@
 package ultra.java.spring.test1.controller;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("message")
 public class MessageController {
+
+    public List<Map<String, String>> messages = new ArrayList<Map<String, String>>(){{
+        add(new HashMap<String, String>(){{put("id", "1"); put("text", "test1"); put("text1", "test1_1");}});
+        add(new HashMap<String, String>(){{put("id", "2"); put("text", "test2");}});
+        add(new HashMap<String, String>(){{put("id", "3"); put("text", "test3");}});
+    }};
+
+
+    @GetMapping
+    public List<Map<String, String>> list(){
+        return messages;
+    }
+
 }
